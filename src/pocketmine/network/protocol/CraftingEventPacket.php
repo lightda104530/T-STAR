c5d15dec73b97a69f8536527d0ccdb5052a147ae
TSRlightda <lightda104530@gmail.com>
2016-10-31 00:58:56 +0800

    Update CraftingEventPacket.php

--- src/pocketmine/network/protocol/CraftingEventPacket.php
+++ src/pocketmine/network/protocol/CraftingEventPacket.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -18,45 +17,33 @@
  * 
  *
 */
-
 namespace pocketmine\network\protocol;
-
 #include <rules/DataPacket.h>
-
-
 class CraftingEventPacket extends DataPacket{
 	const NETWORK_ID = Info::CRAFTING_EVENT_PACKET;
-
 	public $windowId;
 	public $type;
 	public $id;
 	public $input = [];
 	public $output = [];
-
 	public function clean(){
 		$this->input = [];
 		$this->output = [];
 		return parent::clean();
 	}
-
 	public function decode(){
 		$this->windowId = $this->getByte();
-		$this->type = $this->getInt();
+		$this->type = $this->getVarInt();
 		$this->id = $this->getUUID();
-
-		$size = $this->getInt();
+		$size = $this->getUnsignedVarInt();
 		for($i = 0; $i < $size and $i < 128; ++$i){
 			$this->input[] = $this->getSlot();
 		}
-
-		$size = $this->getInt();
+		$size = $this->getUnsignedVarInt();
 		for($i = 0; $i < $size and $i < 128; ++$i){
 			$this->output[] = $this->getSlot();
 		}
 	}
-
 	public function encode(){
-
 	}
-
 }
