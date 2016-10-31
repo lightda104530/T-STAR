103a5dec5d15fd6e0d77c16bc8f921d3857c97a6
TSRlightda <lightda104530@gmail.com>
2016-10-31 17:51:19 +0800

    Update Animal.php

--- src/pocketmine/entity/Animal.php
+++ src/pocketmine/entity/Animal.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -23,15 +22,9 @@ namespace pocketmine\entity;
 
 
 abstract class Animal extends Creature implements Ageable{
-
-	public function initEntity(){
-		parent::initEntity();
-		if($this->getDataProperty(self::DATA_AGEABLE_FLAGS) === null){
-			$this->setDataProperty(self::DATA_AGEABLE_FLAGS, self::DATA_TYPE_BYTE, 0);
-		}
-	}
-
+	
 	public function isBaby(){
-		return $this->getDataFlag(self::DATA_AGEABLE_FLAGS, self::DATA_FLAG_BABY);
+		
+		return $this->getDataFlag(self::DATA_FLAGS, self::DATA_FLAG_BABY);
 	}
 }
