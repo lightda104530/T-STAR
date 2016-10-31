49af8177dcc7b46cddddb5da52ab6091a1f2755b
TSRlightda <lightda104530@gmail.com>
2016-10-31 18:07:19 +0800

    Create EnderPearl.php

---
+++ src/pocketmine/item/EnderPearl.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ *
+ * ________              ________                         _________     
+ *｜　　　 ｜ / ￣￣￣|  ｜　　　  ｜     /￣￣￣\          |   ___   |
+ * ￣|　|￣  | /￣￣      ￣|　|￣      / /￣￣\  \        |  |___|  |
+ *   |  |    \ \_____      |  |      /  /     \  \       |      ___| 
+ *   |  |     \_____  \    |  |     /  /_______\  \      |  |\  \
+ *   |  |           | |    |  |    /               \     |  | \  \
+ *   |  |      _____/ |    |  |   /  /￣￣￣￣￣￣\  \    |  |  \  \
+ *   |__|     |_______/    |__|  /  /             \  \   |__|   \__\
+ *
+ *
+ *
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author DockCreaTer Team
+ * @link http://www.github.com/DockCreaTer/
+ * 
+ *
+*/
+namespace pocketmine\item;
+
+class EnderPearl extends Item{
+	public function __construct($meta = 0, $count = 1){
+		parent::__construct(self::ENDERPEARL, 0, $count, "Enderpearl");
+	}
+	public function getMaxStackSize() : int {
+		return 16;
+	}
+}
