9054d21483fb9316837983e524ab0d378d58e978
TSRlightda <lightda104530@gmail.com>
2016-10-31 17:49:28 +0800

    Update VersionCommand.php

--- src/pocketmine/command/defaults/VersionCommand.php
+++ src/pocketmine/command/defaults/VersionCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____
@@ -18,17 +17,13 @@
  *
  *
 */
-
 namespace pocketmine\command\defaults;
-
 use pocketmine\command\CommandSender;
 use pocketmine\event\TranslationContainer;
 use pocketmine\network\protocol\Info;
 use pocketmine\plugin\Plugin;
 use pocketmine\utils\TextFormat;
-
 class VersionCommand extends VanillaCommand{
-
 	public function __construct($name){
 		parent::__construct(
 			$name,
@@ -38,12 +33,10 @@ class VersionCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.version");
 	}
-
 	public function execute(CommandSender $sender, $currentAlias, array $args){
 		if(!$this->testPermission($sender)){
 			return \true;
 		}
-
 		if(\count($args) === 0){
 			$sender->sendMessage(new TranslationContainer("pocketmine.server.info.extended.title"));
 			$sender->sendMessage(new TranslationContainer("pocketmine.server.info.extended1", [
@@ -56,7 +49,7 @@ class VersionCommand extends VanillaCommand{
 			]));
 			$sender->sendMessage(new TranslationContainer("pocketmine.server.info.extended3", [
 											$sender->getServer()->getApiVersion(),
-											$sender->getServer()->getiTXApiVersion()
+											$sender->getServer()->getH4PMApiVersion()
 			
 			]));
 			$sender->sendMessage(new TranslationContainer("pocketmine.server.info.extended4", [
@@ -66,13 +59,10 @@ class VersionCommand extends VanillaCommand{
 		}else{
 			$pluginName = \implode(" ", $args);
 			$exactPlugin = $sender->getServer()->getPluginManager()->getPlugin($pluginName);
-
 			if($exactPlugin instanceof Plugin){
 				$this->describeToSender($exactPlugin, $sender);
-
 				return \true;
 			}
-
 			$found = \false;
 			$pluginName = \strtolower($pluginName);
 			foreach($sender->getServer()->getPluginManager()->getPlugins() as $plugin){
@@ -81,27 +71,21 @@ class VersionCommand extends VanillaCommand{
 					$found = \true;
 				}
 			}
-
 			if(!$found){
 				$sender->sendMessage(new TranslationContainer("pocketmine.command.version.noSuchPlugin"));
 			}
 		}
-
 		return \true;
 	}
-
 	private function describeToSender(Plugin $plugin, CommandSender $sender){
 		$desc = $plugin->getDescription();
 		$sender->sendMessage(TextFormat::DARK_GREEN . $desc->getName() . TextFormat::WHITE . " version " . TextFormat::DARK_GREEN . $desc->getVersion());
-
 		if($desc->getDescription() != \null){
 			$sender->sendMessage($desc->getDescription());
 		}
-
 		if($desc->getWebsite() != \null){
 			$sender->sendMessage("Website: " . $desc->getWebsite());
 		}
-
 		if(\count($authors = $desc->getAuthors()) > 0){
 			if(\count($authors) === 1){
 				$sender->sendMessage("Author: " . \implode(", ", $authors));
